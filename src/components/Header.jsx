d1ca2505e693c5f80c2a215bac2477f9a602a275
Group One <groupone@gmail.com>
2022-02-14 14:40:15 +0100

    working on the navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const HEADER = () => {
+    return ( 
+        <div className="Container">
+            <div className="Navber_container">
+                
+            </div>
+            <div className="Headerbody_container"></div>
+            <div className="Headerfooter_container"></div>
+        </div>
+     );
+}
+ 
+export default HEADER;
